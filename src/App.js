099179b0221889d5ae12aa6c5538eaade758c878
WZDMATL <wzdm315@gmail.com>
2021-07-25 02:33:45 -0400

    Added a search input box and temporary css styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import logo from "./dictionaryapplogo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>placeholder text</p>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by WZDM Management Agency</footer>
+      </div>
     </div>
   );
 }
